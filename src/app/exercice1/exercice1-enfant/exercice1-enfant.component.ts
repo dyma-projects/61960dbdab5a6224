bb0e525383ee813dc6777afb84d1375f985367e7
Hbm-Dev Open <scb66695@open-groupe.com>
2022-04-01 10:40:21 +0200

    Commit exercices done

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice1-enfant',
-  templateUrl: './exercice1-enfant.component.html',
-  styleUrls: ['./exercice1-enfant.component.css']
+  selector: "app-exercice1-enfant",
+  templateUrl: "./exercice1-enfant.component.html",
+  styleUrls: ["./exercice1-enfant.component.css"],
 })
 export class Exercice1EnfantComponent implements OnInit {
+  @Input() compteur = 0;
+  @Output() buttonEvent = new EventEmitter<number>();
 
-  constructor() { }
+  constructor() {}
+  ngOnInit() {}
 
-  ngOnInit() {
+  onClickButton(value: number) {
+    this.buttonEvent.emit(value);
+    console.log(this.buttonEvent);
   }
-
 }
